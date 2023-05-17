debd2f2a7b3625d8ae57c6947cb71e8156d3bf78
Stanislaw Findeisen <stanislaw@ik.me>
2023-05-17 23:00:23 +0200

    update the tests

--- src/test/java/com/eisenbits/demo/jmerkletree/MerkleTreeDemoTests.java
+++ src/test/java/com/eisenbits/demo/jmerkletree/MerkleTreeDemoTests.java
@@ -25,16 +25,16 @@ class MerkleTreeDemoTests {
     mt.append(new byte[]{1,2,3,4,5,6,7});
     assertFalse(mt.isEmpty());
     assertEquals(2, mt.leafCount());
-    assertEquals("549af97dc861cdd3f7c651be53f6458a68b87f6a924543ce572dc647d3e040d2", mt.rootHash());
+    assertEquals("f16a90becd1fb59e19e7294e2fc0f549bba07bec57932ab482b2e88c56d84964", mt.rootHash());
 
     mt.append(new byte[]{8,9});
     assertFalse(mt.isEmpty());
     assertEquals(2, mt.leafCount());
-    assertEquals("b0ed8285ccc6984845c1d579fe405d567ed7b14327f78555244b70d771ec98a8", mt.rootHash());
+    assertEquals("23fe91c8398c7a228f2c35a0b9021257f38907d3932ff3e808c61c0c836976b7", mt.rootHash());
 
     mt.updateLeaf(0, new byte[]{20,21,22});
     assertFalse(mt.isEmpty());
     assertEquals(2, mt.leafCount());
-    assertEquals("2eb4c74b7963c4dbcb322bda6cf681b2c8c655f8e7f4650654531b8af6b26330", mt.rootHash());
+    assertEquals("ee6cd779efc6f2b256af3a649d7ec2fa3991cd287cdba9cf119f44c9bf83f289", mt.rootHash());
   }
 }
