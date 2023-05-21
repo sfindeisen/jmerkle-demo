8071835b99bd6eff5c2e9b91d6555fef09a13b0f
Stanislaw Findeisen <stanislaw@ik.me>
2023-05-21 12:13:58 +0200

    add a test case

--- src/test/java/com/eisenbits/demo/jmerkletree/MerkleTreeDemoTests.java
+++ src/test/java/com/eisenbits/demo/jmerkletree/MerkleTreeDemoTests.java
@@ -22,7 +22,6 @@ class MerkleTreeDemoTests {
     assertEquals(0, mt.leafCount());
   }
 
-
   @Test
   void smallTree() throws NoSuchAlgorithmException {
     MessageDigest digest = makeDigest();
@@ -45,5 +44,19 @@ class MerkleTreeDemoTests {
     assertFalse(mt.isEmpty());
     assertEquals(2, mt.leafCount());
     assertEquals("ee6cd779efc6f2b256af3a649d7ec2fa3991cd287cdba9cf119f44c9bf83f289", mt.rootHash());
+  }
+
+  @Test
+  void tree20() throws NoSuchAlgorithmException {
+    MessageDigest digest = makeDigest();
+    MerkleTree mt = new MerkleTree(digest, 3);
+
+    mt.append(new byte[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
+    assertEquals(7, mt.leafCount());
+    assertEquals("257430c9dc285855d7c5297172e40c7bfdcde80f17a6e65efa5630078cd50969", mt.rootHash());
+
+    mt.append(new byte[]{8,9});
+    assertEquals(8, mt.leafCount());
+    assertEquals("43697f42a670936e3ed6c1f8cd83869c3e0640243384b49574bea24d2575fb4a", mt.rootHash());
   }
 }
